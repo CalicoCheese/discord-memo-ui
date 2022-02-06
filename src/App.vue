7464da89d432d726c34c4fdaa99922863d516fec
chick_0 <chick_0@ch1ck.xyz>
2022-02-06 17:53:51 +0900

    add spinner for api request

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,61 @@
     <router-view />
 </template>
 
+<script>
+import { ref, watch } from "vue";
+import { useLoading } from "vue-loading-overlay";
+import axios from "axios";
+
+export default {
+    setup() {
+        // 스피너 객체를 보관하는 변수
+        const loading = useLoading();
+        let loadingObj = undefined;
+
+        // 스피너가 돌아야 하는지 여부를 저장하는 변수
+        const spinner = ref();
+
+        // 스피너 핸들러
+        watch(spinner, (need_spin) => {
+            if (need_spin) {
+                loadingObj = loading.show({
+                    color: "#FFCC4D",
+                    canCancel: false,
+                });
+            } else {
+                if (loadingObj != undefined) {
+                    loadingObj.hide();
+                }
+            }
+        });
+
+        // axios 요청 시작시 스피너 보이기
+        // 오류나거나 요청이 끝나면 스피터 숨기기
+        axios.interceptors.request.use(
+            (config) => {
+                spinner.value = true;
+                return config;
+            },
+            (error) => {
+                spinner.value = false;
+                return Promise.reject(error);
+            }
+        );
+
+        axios.interceptors.response.use(
+            (response) => {
+                spinner.value = false;
+                return response;
+            },
+            (error) => {
+                spinner.value = false;
+                return Promise.reject(error);
+            }
+        );
+    },
+};
+</script>
+
 <style>
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
