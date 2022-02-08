2682c80b26198a2b3d87107c1b6eabe3fcf8e435
chick_0 <chick_0@ch1ck.xyz>
2022-02-09 05:09:08 +0900

    add redirect to discord oauth

--- src/views/Auth/Move.vue
+++ src/views/Auth/Move.vue
@@ -4,3 +4,30 @@
         <p>로그인 페이지로 이동중입니다...</p>
     </div>
 </template>
+
+<script>
+import axios from "axios";
+import Swal from "sweetalert2";
+import config from "@/config";
+
+export default {
+    setup() {
+        axios({
+            url: `${config.api.host}/auth/get-url`,
+        })
+            .then((e) => {
+                const data = e.data;
+                const url = data.data.auth;
+
+                window.location.replace(url);
+            })
+            .catch((e) => {
+                const data = e.response.data;
+                Swal.fire({
+                    title: data.meta.code,
+                    text: data.meta.message,
+                });
+            });
+    },
+};
+</script>
