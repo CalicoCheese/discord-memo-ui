b579352d8a99e326caf7dff578760351b8a1a1ea
chick_0 <chick_0@ch1ck.xyz>
2022-02-09 05:29:13 +0900

    oauth callback

--- src/config.js
+++ src/config.js
@@ -2,4 +2,7 @@ export default {
     api: {
         host: "http://localhost:5000",
     },
+    token: {
+        key: "dmui-token",
+    },
 };
--- src/views/Auth/Callback.vue
+++ src/views/Auth/Callback.vue
@@ -1,19 +1,55 @@
 <template>
-    <h1>
-        Callback Code is <b>{{ code }}</b>
-    </h1>
+    <div>
+        <h1>잠시만요...</h1>
+        <p>로그인 요청 처리중입니다...</p>
+    </div>
 </template>
 
 <script>
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
+import axios from "axios";
+import Swal from "sweetalert2";
+import config from "@/config";
 
 export default {
     setup() {
         const route = useRoute();
+        const router = useRouter();
+        const code = route.query.code;
 
-        return {
-            code: route.query.code,
-        };
+        if (code == undefined) {
+            Swal.fire({
+                title: "?",
+                text: "올바른 접근이 아닙니다.",
+            }).then(() => {
+                router.push({ name: "Home" });
+            });
+        } else {
+            axios({
+                url: `${config.api.host}/auth/callback?code=${code}`,
+            })
+                .then((e) => {
+                    const data = e.data;
+
+                    localStorage.setItem(config.token.key, data.data.token);
+
+                    Swal.fire({
+                        title: data.meta.code,
+                        text: data.meta.message,
+                    }).then(() => {
+                        router.push({ name: "Memo" });
+                    });
+                })
+                .catch((e) => {
+                    const data = e.response.data;
+                    Swal.fire({
+                        title: data.meta.code,
+                        text: data.meta.message,
+                    }).then(() => {
+                        router.push({ name: "Home" });
+                    });
+                });
+        }
     },
 };
 </script>
