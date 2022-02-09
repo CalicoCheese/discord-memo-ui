cfec9d926df35479f3dbef9b54ad90212220e543
chick_0 <chick_0@ch1ck.xyz>
2022-02-10 00:50:14 +0900

    add style

--- src/views/Auth/Callback.vue
+++ src/views/Auth/Callback.vue
@@ -1,8 +1,10 @@
 <template>
-    <div>
-        <h1>잠시만요...</h1>
-        <p>로그인 요청 처리중입니다...</p>
-    </div>
+    <section class="section">
+        <div class="container">
+            <h1 class="title is-1">잠시만요...</h1>
+            <p>로그인 요청 처리중입니다...</p>
+        </div>
+    </section>
 </template>
 
 <script>
