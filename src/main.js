93a186e908d9b306e8eaa0e4bf695ac9ea18e347
chick_0 <chick_0@ch1ck.xyz>
2022-02-10 00:48:35 +0900

    forgot to import

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "vue-loading-overlay/dist/vue-loading.css";
 import "./index.sass";
 
 createApp(App).use(router).mount("#app");
