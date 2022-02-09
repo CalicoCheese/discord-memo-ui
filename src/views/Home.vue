4906a8e00373074d1357e4e587bd349ee004a82e
chick_0 <chick_0@ch1ck.xyz>
2022-02-09 23:24:02 +0900

    move to memo route
    if user is logined

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,3 +4,31 @@
         <p>메모 서비스 개발중입니다...</p>
     </div>
 </template>
+
+<script>
+import Swal from "sweetalert2";
+import { useRouter } from "vue-router";
+import { login } from "@/utils";
+
+export default {
+    setup() {
+        if (login()) {
+            const router = useRouter();
+
+            Swal.fire({
+                icon: "question",
+                title: "반가워요!",
+                text: "메모를 확인하러 갈까요?",
+                confirmButtonText: "네!",
+                showCloseButton: true,
+                timer: 2022,
+                timerProgressBar: true,
+            }).then((e) => {
+                if (e.isConfirmed) {
+                    router.push({ name: "Memo" });
+                }
+            });
+        }
+    },
+};
+</script>
