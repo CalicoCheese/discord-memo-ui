0cbdbf36c564d2098ab71d604269f604243ad217
chick_0 <chick_0@ch1ck.xyz>
2022-02-10 01:43:23 +0900

    add login alert

--- src/views/Memo.vue
+++ src/views/Memo.vue
@@ -1,5 +1,37 @@
 <template>
-    <div class="memo">
-        <h1>This is an Memo page</h1>
-    </div>
+    <section class="section" v-if="show == true">
+        <div class="container">
+            <p>TODO:메모 api 완성후 작업</p>
+        </div>
+    </section>
 </template>
+
+<script>
+import Swal from "sweetalert2";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import { login } from "@/utils";
+
+export default {
+    setup() {
+        const router = useRouter();
+        const show = ref(false);
+
+        if (!login()) {
+            Swal.fire({
+                icon: "warning",
+                text: "메모를 확인하려면 로그인해야 합니다.",
+                timer: 2022,
+                timerProgressBar: true,
+                showConfirmButton: false,
+            }).then(() => {
+                router.push({ name: "Home" });
+            });
+        }
+
+        return {
+            show,
+        };
+    },
+};
+</script>
