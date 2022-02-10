605d110d25e95ad15dded9ba2293dcf82fb558c3
chick_0 <chick_0@ch1ck.xyz>
2022-02-11 03:23:59 +0900

    update home route

--- src/config.js
+++ src/config.js
@@ -6,4 +6,13 @@ export default {
         key: "dmui-token",
         exp: "dmui-exp",
     },
+    home: {
+        email: "mailto:team@calicocheese.xyz",
+        github: {
+            profile: "https://github.com/CalicoCheese",
+            api: "https://github.com/CalicoCheese/discord-memo-api",
+            ui: "https://github.com/CalicoCheese/discord-memo-ui",
+            bot: "https://github.com/CalicoCheese/discord-memo-bot",
+        },
+    },
 };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,37 +1,82 @@
 <template>
     <section class="section">
         <div class="container">
-            <h1 class="title is-1">개발중</h1>
-            <p>
-                <a
-                    href="https://github.com/CalicoCheese/discord-memo-ui"
-                    target="_blank"
-                >
-                    discord-memo-ui
-                </a>
-            </p>
-            <p>
-                <a
-                    href="https://github.com/CalicoCheese/discord-memo-api"
-                    target="_blank"
-                >
-                    discord-memo-api
-                </a>
-            </p>
+            <h1 class="title is-1">메모</h1>
+            <div class="content is-large">
+                <p>디스코드 봇한테 DM을 보내면 메모가 등록됩니다.</p>
+            </div>
+
+            <router-link
+                class="button is-link is-medium"
+                v-if="isLogin == false"
+                :to="{ name: 'Auth' }"
+            >
+                로그인
+            </router-link>
+            <router-link
+                class="button is-dark is-medium"
+                v-else-if="isLogin == true"
+                :to="{ name: 'Logout' }"
+            >
+                로그아웃
+            </router-link>
+
+            <router-link
+                class="button is-info is-medium"
+                :to="{ name: 'Invite' }"
+                target="_blank"
+            >
+                봇 초대하기
+            </router-link>
         </div>
     </section>
 
     <section class="section">
         <div class="container">
-            <router-link class="button is-info" :to="{ name: 'Auth' }">
-                로그인
-            </router-link>
-            &nbsp;
-            <router-link class="button is-dark" :to="{ name: 'Logout' }">
-                로그아웃
-            </router-link>
+            <h1 class="title is-2">암호화</h1>
+            <div class="content is-large"></div>
+        </div>
+    </section>
 
-            <p>* 로컬환경에서 개발서버를 켜야합니다.</p>
+    <section class="section">
+        <div class="container">
+            <h1 class="title is-2">오픈소스!</h1>
+            <div class="content is-large">
+                <blockquote>
+                    개발이 완료되지 않는 프로젝트는 비공개 상태입니다.
+                </blockquote>
+                <p>
+                    이 메모 프로젝트는 오픈소스 프로젝트 입니다. 다음 링크를
+                    확인해주세요.
+                </p>
+                <ul>
+                    <li>
+                        <a :href="home.github.ui">UI</a>
+                    </li>
+                    <li>
+                        <a :href="home.github.api">API</a>
+                    </li>
+                    <li>
+                        <a :href="home.github.bot">BOT</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+
+    <section class="section">
+        <div class="container">
+            <h1 class="title is-2">버그 &amp; 피드백</h1>
+            <div class="content is-large">
+                <p>
+                    사용중 버그를 발견했거나 요청하고 싶은 기능이 있다면 다음
+                    <a :href="home.email">이메일</a>로 연락해주세요.
+                </p>
+                <p>
+                    또는 <a :href="home.github.profile">Github</a>에 이슈를
+                    남겨주세요.
+                </p>
+            </div>
         </div>
     </section>
 </template>
@@ -40,6 +85,7 @@
 import Swal from "sweetalert2";
 import { useRouter } from "vue-router";
 import { login } from "@/utils";
+import config from "@/config";
 
 export default {
     setup() {
@@ -60,6 +106,17 @@ export default {
                 }
             });
         }
+
+        return {
+            isLogin: login(),
+            home: config.home,
+        };
     },
 };
 </script>
+
+<style scoped>
+.button {
+    margin-right: 5px;
+}
+</style>
