81950250cf6b78de68bc9d25a00b2e10d866668d
chick_0 <chick_0@ch1ck.xyz>
2022-02-25 00:24:29 +0900

    update agree page

--- src/views/Auth/Update.vue
+++ src/views/Auth/Update.vue
@@ -4,39 +4,45 @@
             <h1 class="title is-1">사용자 등록</h1>
             <div class="content is-large">
                 <p>
-                    메모 서비스를 이용하려면 서비스 이용약관과 개인정보
-                    처리방침을 읽고 동의해야합니다.
+                    메모 서비스를 이용하려면
+                    <b class="has-text-link">서비스 이용약관</b>과
+                    <b class="has-text-link">개인정보 처리방침</b>을 읽고
+                    동의해야합니다.
                 </p>
             </div>
+        </div>
+    </section>
 
+    <section class="section">
+        <div class="container">
+            <h1 class="title is-1">서비스 이용약관</h1>
+            <div class="content" v-html="tos"></div>
             <div class="field">
                 <div class="control">
                     <label class="checkbox">
                         <input type="checkbox" v-model="tos_agree" />
                         나는
-                        <router-link
-                            :to="{ name: 'ToS.View' }"
-                            target="_blank"
-                            @click="tos_read = true"
-                        >
-                            서비스 이용약관
-                        </router-link>
-                        을 읽었으며 동의합니다.
+                        <b class="has-text-link">서비스 이용약관</b>을
+                        <b class="has-text-info">확인</b>하였으며
+                        <b class="has-text-info">동의</b>합니다.
                     </label>
                 </div>
+            </div>
+        </div>
+    </section>
 
-                <div class="control is-medium">
+    <section class="section">
+        <div class="container">
+            <h1 class="title is-1">개인정보 처리방침</h1>
+            <div class="content" v-html="privacy"></div>
+            <div class="field">
+                <div class="control">
                     <label class="checkbox">
                         <input type="checkbox" v-model="pri_agree" />
                         나는
-                        <router-link
-                            :to="{ name: 'Privacy.View' }"
-                            target="_blank"
-                            @click="pri_read = true"
-                        >
-                            개인정보 처리방침
-                        </router-link>
-                        을 읽었으며 동의합니다.
+                        <b class="has-text-link">개인정보 처리방침</b>을
+                        <b class="has-text-info">확인</b>하였으며
+                        <b class="has-text-info">동의</b>합니다.
                     </label>
                 </div>
             </div>
@@ -47,7 +53,10 @@
         <div class="container">
             <div class="flied">
                 <div class="control">
-                    <button class="button is-link is-medium" @click="on_next()">
+                    <button
+                        class="button is-link is-large is-fullwidth"
+                        @click="on_next()"
+                    >
                         다음
                     </button>
                 </div>
@@ -59,30 +68,56 @@
 <script>
 import { ref } from "vue";
 import { useRouter } from "vue-router";
+import { parse } from "marked";
 import axios from "axios";
 import Swal from "sweetalert2";
 import { api } from "@/config";
 import { getToken, login } from "@/utils";
+import { defaultError } from "@/utils";
 
 export default {
     name: "auth-update",
     setup() {
         const router = useRouter();
-        const tos_agree = ref(false);
-        const pri_agree = ref(false);
-        const tos_read = ref(false);
-        const pri_read = ref(false);
 
         if (!login()) {
-            Swal.fire({
-                icon: "error",
-                text: "올바른 접근이 아닙니다.",
-                timer: 2022,
-                timerProgressBar: true,
-            }).then(() => {
-                router.push({ name: "Home" });
-            });
+            router.push({ name: "Home" });
+            return {};
         }
+
+        // flag
+        const tos_agree = ref(false);
+        const pri_agree = ref(false);
+
+        // html
+        const tos = ref("<p>잠시만요! 불러오고 있습니다...</p>");
+        const privacy = ref("<p>잠시만요! 불러오고 있습니다...</p>");
+
+        const fetchHTML = () => {
+            axios({
+                method: "GET",
+                baseURL: api.host,
+                url: `/tos`,
+            })
+                .then((resp) => {
+                    const data = resp.data;
+                    tos.value = parse(data.data.text);
+                })
+                .catch((err) => defaultError(err));
+
+            axios({
+                method: "GET",
+                baseURL: api.host,
+                url: `/privacy`,
+            })
+                .then((resp) => {
+                    const data = resp.data;
+                    privacy.value = parse(data.data.text);
+                })
+                .catch((err) => defaultError(err));
+        };
+
+        fetchHTML();
 
         const on_next = () => {
             const fire = (message) => {
@@ -96,12 +131,8 @@ export default {
 
             if (!tos_agree.value) {
                 fire("서비스 이용약관에 동의해야합니다.");
-            } else if (!tos_read.value) {
-                fire("서비스 이용약관을 확인해야합니다.");
             } else if (!pri_agree.value) {
                 fire("개인정보 처리방침에 동의해야합니다.");
-            } else if (!pri_read.value) {
-                fire("개인정보 처리방침을 확인해야합니다.");
             } else {
                 axios({
                     method: "POST",
@@ -151,12 +182,18 @@ export default {
         };
 
         return {
+            tos,
+            privacy,
             tos_agree,
             pri_agree,
-            tos_read,
-            pri_read,
             on_next,
         };
     },
 };
 </script>
+
+<style scoped>
+label {
+    font-size: 20px;
+}
+</style>
