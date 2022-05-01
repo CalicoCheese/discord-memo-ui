21090be15df25c59e646757da921665a352b7883
chick_0 <chick_0@ch1ck.xyz>
2022-05-01 20:58:08 +0900

    use css

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "vue-loading-overlay/dist/vue-loading.css";
-import "./index.sass";
+import "@/css/bulma.min.css";
 
 createApp(App).use(router).mount("#app");
